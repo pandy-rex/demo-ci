302fa1f9efbede1e5f9b7c7cb4250e5d3e77f211
Evgeniy Panchenko <evgeniy.panchenko@onyxenterprises.com>
2018-12-09 14:30:28 +0200

    fix composer autoload

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -4,7 +4,7 @@ TOKEN=fe55516a96e6b66779b9fc1101bdcc47a9fa388d
 
 curl --user $TOKEN: \
     --request POST \
-    --form revision=6be40f5c20339e4ab0f5e8578016fb7e1410824b \
+    --form revision=d624c958230c59eced626778832138a5f2465789 \
     --form config=@config.yml \
     --form notify=false \
     https://circleci.com/api/v1.1/project/github/pandy-rex/demo-ci/tree/master
